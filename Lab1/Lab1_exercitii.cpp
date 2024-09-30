b435efad294c03e4827eb0fa9347a5891dd0f7f9
AlexandruNicu2000 <163536851+AlexandruNicu2000@users.noreply.github.com>
2024-09-30 09:55:22 +0300

    Lab1_exercitii.cpp

--- Lab1/Lab1_exercitii.cpp
+++ Lab1/Lab1_exercitii.cpp
@@ -1,76 +1,88 @@
 #include <iostream>
 #include <string>
 
+
+//Mostenire
 class Vehicle {
+protected:
+    std::string marca;
 public:
-    Vehicle(const std::string& mark) : marca_(mark) {}
+    Vehicle(const std::string& mark) : marca(mark) {}
 
     void displayMark() const {
-        std::cout << "Marca vehiculului: " << marca_ << std::endl;
+        std::cout << "Marca vehiculului: " << marca << std::endl;
     }
 
-protected:
-    std::string marca_;
-};
 
+};
 
 class Car : public Vehicle {
+private:
+    int an;
+
 public:
-    Car(const std::string& marca, int year) : Vehicle(marca), an_(year) {}
+    Car(const std::string& marca, int year) : Vehicle(marca), an(year) {}
 
     void displayVehicleDetails() const {
-        std::cout << "Masina " << marca_ << " este din anul " << an_ << std::endl;
+        std::cout << "Masina " << marca << " este din anul " << an << std::endl;
     }
-
-private:
-    int an_;
 };
 
 
+//Destructor
 class Person {
+private:
+    std::string nume;
+    int age;
 public:
-    Person(const std::string& name, int age)
-        : nume_(name), age_(age) {
-        std::cout << "Constructor pentru " << nume_ << ", " << age_ << " ani.\n";
-    }
+    Person(const std::string& name, int age): nume(name), age(age) {}
 
     ~Person() {
-        std::cout << "Destructor pentru " << nume_ << ".\n";
+        std::cout << "Destructor pentru " << nume << ".\n";
     }
-
-private:
-    std::string nume_;
-    int age_;
 };
 
+//Friend
 class BankAccount {
-public:
-    BankAccount(const std::string& name, double balance)
-        : nume_(name), sold_(balance) {}
-
-    friend void displayBankDetails(const BankAccount& cont);
-
 private:
-    std::string nume_;
-    double sold_;
+    std::string ID;
+    double balance;
+public:
+    BankAccount(const std::string& name, double balance): ID(name), balance(balance) {}
+
+    friend void displayBankDetails(const BankAccount& account);
+
+    friend class BankClient;
 };
 
-void displayBankDetails(const BankAccount& cont) {
-    std::cout << "Nume: " << cont.nume_ << "\n";
-    std::cout << "Sold: " << cont.sold_ << "\n";
+class BankClient {
+private:
+    std::string clientName;
+public:
+    BankClient(const std::string& clientName): clientName(clientName) {}
+    void displayClientBalance(const BankAccount& account);
+};
+
+void displayBankDetails(const BankAccount& account) {
+    std::cout << "ID: " << account.ID << "\n";
 }
 
+void BankClient::displayClientBalance(const BankAccount& account) {
+    std::cout << "Account balance: " << account.balance << std::endl;
+}
+
+
 int main() {
-    Vehicle vehicul("General");
-    Car masina("Dacia", 2023);
+    Vehicle vehicle("General");
+    Car car("Dacia", 2023);
 
-    vehicul.displayMark();
-    masina.displayVehicleDetails();
-
+    vehicle.displayMark();
+    car.displayVehicleDetails();
 
     Person p("Ion", 30);
 
-
-    BankAccount cont("Ana", 1500.0);
-    displayBankDetails(cont);
+    BankClient client("Ana");
+    BankAccount account("1313131312", 1500.0);
+    displayBankDetails(account);
+    client.displayClientBalance(account);
 }
